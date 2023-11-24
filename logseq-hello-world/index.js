5fe286c18834a74c758d705e0641d9736f59e3ae
rd3222 <114133959+rd3222@users.noreply.github.com>
2023-11-24 08:41:01 +0000

    updated to work with pplx-70

--- logseq-hello-world/index.js
+++ logseq-hello-world/index.js
@@ -1,9 +1,43 @@
-/**
- * entry
- */
-function main () {
-  logseq.App.showMsg('❤️ Message from Hello World Plugin :)')
+const axios = require('axios');
+
+// Your pplx-api key
+const apiKey = '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';
+
+// The pplx-api endpoint
+const apiEndpoint = 'https://api.perplexity.ai';
+
+// The Logseq API endpoint
+const logseqApiEndpoint = 'http://localhost:6807';
+
+// A function to make a request to the pplx-api
+async function makeRequestToPplxApi(prompt) {
+  const response = await axios.post(`${apiEndpoint}/v1/models/pplx-70b/completions`, {
+    prompt: prompt,
+  }, {
+    headers: {
+      'Authorization': `Bearer ${apiKey}`
+    }
+  });
+
+  return response.data;
 }
 
-// bootstrap
-logseq.ready(main).catch(console.error)
+// A function to send a command to the Logseq API
+async function sendCommandToLogseqApi(command) {
+  const response = await axios.post(`${logseqApiEndpoint}/api/command`, {
+    command: command,
+  });
+
+  return response.data;
+}
+
+// A function to create a connection between pplx-70b and Logseq
+async function connectPplxToLogseq(prompt) {
+  const pplxResponse = await makeRequestToPplxApi(prompt);
+  const logseqResponse = await sendCommandToLogseqApi(pplxResponse);
+
+  return logseqResponse;
+}
+
+// Use the function
+connectPplxToLogseq('Hello, world!');
